15a7c499380a8770565de649ed63db79b5db9607
Chris Killian <ckillian@css.edu>
2018-09-18 10:37:25 -0500

    Added MulitpleChoiceQuestion and TrueFalseQuestion classes.

---
+++ SimpleQuizApp/MultipleChoiceQuestion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQuizApplication
+{
+    class MultipleChoiceQuestion : Question
+    {
+        String[] possibleAnswers;
+        int answerIndex;
+
+        public MultipleChoiceQuestion(String questionText, String[] possibleAnswers, int answerIndex) : base(questionText)
+        {
+            this.possibleAnswers = possibleAnswers;
+            this.answerIndex = answerIndex;
+        }
+
+        /**
+         * getQuestion()
+        * @return Adds newline after each possible answer
+        */
+
+        public override string getQuestion()
+        {
+            String fullQuestion = questionText + "\n";
+
+            foreach (String s in possibleAnswers)
+            {
+                fullQuestion += s + "\n";
+            }
+
+            return fullQuestion;
+        }
+
+        public override string getAnswer()
+        {
+            return possibleAnswers[answerIndex];
+        }
+
+
+        public override bool checkAnswer(String answer)
+        {
+            return (answer.Equals(possibleAnswers[answerIndex]));
+        }
+    }
+}
--- SimpleQuizApp/SimpleQuizModel.cs
+++ SimpleQuizApp/SimpleQuizModel.cs
@@ -27,23 +27,22 @@ namespace SimpleQuizApplication
             myQuestions.Add(new ShortAnswerQuestion(
                     "What is the name of Jerry Lee Lewis's biggest solid gold hit?",
                     "Great Balls of Fire"));
-            //ADD MORE EXAMPLE QUESTIONS HERE
-            /*
-            myQuestions.add(new ShortAnswerQuestion(
+
+            myQuestions.Add(new ShortAnswerQuestion(
                     "What is the Minnesota state bird?",
                     "Loon"));
-            myQuestions.add(new TrueFalseQuestion(
+            myQuestions.Add(new TrueFalseQuestion(
                    "St. Scholatsica is over 100 years old",
                    "True"));
             String[] possibleAnswers = { "Dave Vosen", "Jen Rosato", "Tom Buck", "Tom Gibbons" };
-            myQuestions.add(new MultipleChoiceQuestion(
+            myQuestions.Add(new MultipleChoiceQuestion(
                     "Which faculty member owns a number of bee hives?",
-                    possibleAnswers,
-                    2));
-            */
+                   possibleAnswers,
+                   2));
+
 
             //Shuffle the questions and set the starting one to zero
-            // Collections.shuffle(myQuestions);            // C# does not have a clear shuffle option
+            //Collections.shuffle(myQuestions);            // C# does not have a clear shuffle option
             currentQuestion = 0;
         }
 
